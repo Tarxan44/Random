f4b62713abdae2d4f27788b258d9f3ad93b1b9a5
waterbuffalo42 <32183503+waterbuffalo42@users.noreply.github.com>
2020-08-04 00:36:59 -0500

    updated category name list, second attempt on retrieving num mentioned

--- categorySort/notebooks/functions/CategorySort.py
+++ categorySort/notebooks/functions/CategorySort.py
@@ -4,8 +4,9 @@ class CategorySort():
    category_frequency('ye','Code description')
 
    def category_frequency(path, target_variable):
-      """ Using the orginal BLS excel file, find categories, number of times used, years active and inactive """
-      #used for testing - to remove - write to an excel file
+      """ Using the orginal BLS excel file, find categories, number of times used, years active and inactive 
+                                             ***CHECK PATH***                                            """
+      #used for testing - to remove - write to an excel file 
       writer = pd.ExcelWriter('notebooks/data/test.xlsx')
 
       #Write to a file to check outputs
@@ -28,16 +29,19 @@ class CategorySort():
 
       #Read sheet in and replace NaNs in Last year column with present year
       megaSheet = pd.read_excel(path,sheet_name=2,dtype = dtypes, usecols = list(dtypes))
-      megaSheet["Last year"] = megaSheet["Last year"].fillna(2019)
+      #megaSheet["Last year"] = megaSheet["Last year"].fillna(2019) #doesnt run bc of data type
 
       #creates a DataFrameGroupBys with each category
       mentioned_byCodeDesc = megaSheet.groupby([target_variable])
+      groups = [name for name,unused_df in mentioned_byCodeDesc]
+      mentioned_byCodeDesc = megaSheet.groupby([target_variable])
 
-      #Gets categories in an array
-      keys = megaSheet['Code description'].unique()  
+      #Gets categories in an category
+      finalTable['Category'] = groups #finished up to here - name print in df but nothing else 
       
       #finds number of times each category is used - still need to move those numbers in the final table under 'Times Mentioned'
       '''
+      #Attempt 1
       times_mentioned = megaSheet.groupby([target_variable])
       nameAndActiveDF = pd.DataFrame(codeDesc)
       #active = []
@@ -46,6 +50,24 @@ class CategorySort():
          active =  str(test.iloc[1,0]) + '-' + str(int(test.iloc[2,1])) 
          nameAndActiveDF['Years Active: '] = active
       #print(nameAndActiveDF)
+
+
+      #Attempt 2
+      path = 'data\ce_pumd_interview_diary_dictionary.xlsx'
+      megaSheet = pd.read_excel(path,sheet_name=2,dtype = dtypes, usecols = list(dtypes))
+      #megaSheet["Last year"] = megaSheet["Last year"].fillna(2019)
+      mentioned_byCodeDesc = megaSheet.groupby([target_variable])
+      groups = [name for name,unused_df in mentioned_byCodeDesc]
+      #keys = megaSheet['Code description'].unique()
+      finalTable['Category'] = groups #finished up to here - name print in df but nothing else
+            
+      #focus on getting times mentioned for each category
+      times_mentioned = megaSheet.groupby([target_variable])
+      active = pd.DataFrame(column = 'Years active')
+      counter = 0
+      for mentioned in mentioned, rando_df in times_mentioned:
+         counter = counter +1
+         active
       
       #Close the excel writer at the end
       #writer.save()
