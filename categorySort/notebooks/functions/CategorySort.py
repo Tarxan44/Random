9646d3b20065e410a651af1c284ceb358034112f
waterbuffalo42 <32183503+waterbuffalo42@users.noreply.github.com>
2020-08-03 23:41:58 -0500

    clean up the categorySort.py code, updated the data file available

--- categorySort/notebooks/functions/CategorySort.py
+++ categorySort/notebooks/functions/CategorySort.py
@@ -1,48 +1,53 @@
 from imports import *
+class CategorySort():   
+   #runs the function
+   category_frequency('ye','Code description')
 
-class CategorySort():
-   
-   #  defining the path, and using panda to read it
-   #path = 'notebooks/data/categories.csv'
-   #  future Column names on final output table
-   names = ['Category', 'Times Mentioned', 'Years Absent', 'Years Present']
-   #  for just importing one csv
-   #df = pd.read_csv(path, skiprows=0)
-   
-   #read in csv 
-   
-   counter = 1
-   
-   
+   def category_frequency(path, target_variable):
+      """ Using the orginal BLS excel file, find categories, number of times used, years active and inactive """
+      #used for testing - to remove - write to an excel file
+      writer = pd.ExcelWriter('notebooks/data/test.xlsx')
 
+      #Write to a file to check outputs
+      #df.to_excel(writer)
 
-   def category_frequency(self, path, target_variable):
-      """ Defining category level and 'Times Mentioned' in .csv using a concatinated sheet """
+      #  future Column names on final output table/intitialization stuffs
+      names = ['Category', 'Times Mentioned', 'Years Absent', 'Years Present']
+      finalTable = pd.DataFrame(columns = names)
+
+      #Sets up datatypes and column names to pull from sheet
+      dtypes = {
+         "File" : "category",
+         "Code description" : "category",
+         "First year" : "category",
+         "Last year" : "category"
+      }
+
+      #path to BLS Sheet
+      path = 'notebooks/data/ce_pumd_interview_diary_dictionary.xlsx'
+
+      #Read sheet in and replace NaNs in Last year column with present year
+      megaSheet = pd.read_excel(path,sheet_name=2,dtype = dtypes, usecols = list(dtypes))
+      megaSheet["Last year"] = megaSheet["Last year"].fillna(2019)
+
+      #creates a DataFrameGroupBys with each category
+      mentioned_byCodeDesc = megaSheet.groupby([target_variable])
+
+      #Gets categories in an array
+      keys = megaSheet['Code description'].unique()  
       
-      #  Concatinating all categories sheets in path
-      allSheets = pd.concat([pd.read_csv(f) for f in glob.glob('notebooks/data/categories*.csv')], ignore_index = True)
-
-      #big count sheet
-      bigCountSheet = []
-
-      #read in csv - assumes that there are no other files
-      path = 'notebook/data'
-      categories = os.listdir(path)
-
-      #  defining the level of specificity 
-      target_variable = 'level2'
-
-      bigCountSheet[:1,:] = allSheets.groupby(target_variable).count()
-
-
-      count = 0
-      for x in categories:
-         fullpath = path + categories[x]
-         df = pd.read_csv(fullpath)
-
-         #count how many times 'target_variable' occurs
-         times_mentioned = df.groupby(target_variable).count()
-
-
+      #finds number of times each category is used - still need to move those numbers in the final table under 'Times Mentioned'
+      '''
+      times_mentioned = megaSheet.groupby([target_variable])
+      nameAndActiveDF = pd.DataFrame(codeDesc)
+      #active = []
+      for f in times_mentioned
+         test = times_mentioned.get_group({}).take([5,7], axis = 1)
+         active =  str(test.iloc[1,0]) + '-' + str(int(test.iloc[2,1])) 
+         nameAndActiveDF['Years Active: '] = active
+      #print(nameAndActiveDF)
       
+      #Close the excel writer at the end
+      #writer.save()
+      '''
       
