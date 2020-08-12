0eba73b146f5924f3bfda248f6474e5e386e2317
tarxan44 <mason@addediq.com>
2020-08-12 14:55:18 -0400

    worked in testHierarchies.py to form a possible solution

--- categorySort/notebooks/functions/TestHeiarchies.py
+++ categorySort/notebooks/functions/TestHeiarchies.py
@@ -2,20 +2,21 @@ from imports import *
 class test():
    test_dict = [
       { 
+         """ Will need to manualy make top 4 categories lvl 1 """
          'category_name': 'categories',
          'level_data': [ 
                { 
-                  'level': 1,
+                  'level': 2,
                   'level_name':'Property',
                   'sub_categories': ['Vehicles','Housing Expenses','Physical Assets', 'Real Estate'],
                },
                { 
-                  'level': 2,
+                  'level': 3,
                   'level_name': 'Vehicles',
                   'sub_categories': ['Car','Car Upkeep', 'Other Vehicles'],
                },
                { 
-                  'level': 3,
+                  'level': 4,
                   'level_name': 'Car',
                   'sub_categories': ['Acura', 'Alfa Romeo'],
                }
@@ -23,11 +24,15 @@ class test():
       }
    ]
    df = pd.DataFrame()
-   for cur_dict in test_dict:
+   #for x in df.size()
+   for cur_dict in test_dict: #will become for x in size()...
       for level_data in cur_dict['level_data']:
          cur_level = level_data['level']
          level_name = level_data['level_name']
          for sub_cat in level_data['sub_categories']:
                print("level_name: {0}".format(level_name))
                level4 = sub_cat
-               print("level4: {0}".format(level4))
+               #look for category
+               #assuming the final levels dont have sub categories 
+               #print("level4: {0}".format(level4))
+               print("level" + str(cur_level) + ": {0}".format(level4))
